7d72f016af9027005e3438f4cca226363e0b74de
Cheng Zhang <79437133+BuffTechTalk@users.noreply.github.com>
2024-09-09 15:47:58 -0500

    update code and add image of project

--- webpages/input_code.py
+++ webpages/input_code.py
@@ -1,1 +1,6 @@
 print("hello")
+
+i = 100
+while(i>10):
+    print(f"Value of {i}")
+    i = i-1
--- webpages/project.py
+++ webpages/project.py
@@ -1,4 +1,17 @@
 import streamlit as st
+import time
 
 def project():
     st.title("BuffTeks Project")
+    project_introduction = """
+        Faculty-led coding projects to provide IT solutions and support to problems facing local communities as part of an experiential learning effort.
+    """
+    st.write(project_introduction)
+    
+    st.divider()
+
+    # Project Gallery
+    st.subheader("Project Gallery")
+    st.image("./images/CommunityLinkLogo.svg")
+
+    
