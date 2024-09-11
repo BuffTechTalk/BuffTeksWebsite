22015e4964bcc35f2fd4faf43197a0636887b016
BuffTechTalk <czhang@wtamu.edu>
2024-09-11 18:25:11 -0500

    update student page

--- webpages/outstanding_members.py
+++ webpages/outstanding_members.py
@@ -8,7 +8,7 @@ def outstanding_members():
     #     st.image("./images/StudentDarrian.jpg",width= 300 )
     # with stu1_text:
     #     st.header("Darrian Lambert")
-    #     st.subheader("Senior Undergraduate in Computer Information Systems (CIS)")
+    #     st.subheader("Senior Undergraduate in CIS")
 
     # st.markdown("""
     #         "_Bufftek has been a fantastic experience for me. It gave me the opportunity to apply the skills I’ve learned in class to real-world problems and work alongside others to develop effective software solutions. Through this, I gained hands-on experience with skills that are difficult to practice on your own._
@@ -18,3 +18,18 @@ def outstanding_members():
     #         _The problems I faced throughout the project ranged from finding solutions to everyday inconveniences like finding parking, to developing software for community support. This gave me practice in every phase of developing software from early research and planning, to designing the structure of the application, to building the application, to further refinement and improvement on the application. Overall, it’s been a great experience that I’d recommend to anyone wanting to get into software development or similar fields after graduation._"
         
     #     """)
+
+
+    # st.divider()
+    
+    # stu2_img, stu2_text = st.columns(2)
+    # with stu2_img:
+    #     st.image("./images/StudentKim.jpg",width= 300 )
+    # with stu2_text:
+    #     st.header("Kim Sundblom")
+    #     st.subheader("Senior Undergraduate in CIS")
+    
+
+    # st.markdown("""
+    # _"Bufftek is amazing, as it has given me practical experience with a range of technologies while improving my teamwork and communication skills. I truly feel the club has been instrumental in boosting my confidence and preparing me for my future career by providing a supportive environment where I can apply my skills, seek guidance, and work on collaborative projects with other members. I'm really glad I stepped out of my comfort zone and joined, as it has been an extremely rewarding experience!"_
+    # """)
