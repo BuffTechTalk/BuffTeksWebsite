2020aafe7455e011fde410bc0c8195aaad505d6c
Cheng Zhang <79437133+BuffTechTalk@users.noreply.github.com>
2024-09-17 11:58:46 -0500

    update homepage

--- webpages/navigation.py
+++ webpages/navigation.py
@@ -27,7 +27,7 @@ def navigation_bar():
                     sac.MenuItem('Lesson3', icon='3-square'),
                     ]),       
             ]),
-            sac.MenuItem("Testing", icon='fingerprint'),
+            # sac.MenuItem("Testing", icon='fingerprint'),
             sac.MenuItem(type='divider'),
             sac.MenuItem('Link', type='group', children=[
                 sac.MenuItem('Join Us', icon='person-plus', href='https://wtamuuw.az1.qualtrics.com/jfe/form/SV_2boQtKLCptO33HE'),
--- webpages/outstanding_members.py
+++ webpages/outstanding_members.py
@@ -2,34 +2,34 @@ import streamlit as st
 
 def outstanding_members():
     st.title('Outstanding Members')
-    st.write("Under Construction")
-    # stu1_img, stu1_text = st.columns(2)
-    # with stu1_img:
-    #     st.image("./images/StudentDarrian.jpg",width= 300 )
-    # with stu1_text:
-    #     st.header("Darrian Lambert")
-    #     st.subheader("Senior Undergraduate in CIS")
+    # st.write("Under Construction")
+    stu1_img, stu1_text = st.columns(2)
+    with stu1_img:
+        st.image("./images/StudentDarrian.jpg",width= 200 )
+    with stu1_text:
+        st.header("Darrian Lambert")
+        st.subheader("Senior Student in CIS")
 
-    # st.markdown("""
-    #         "_Bufftek has been a fantastic experience for me. It gave me the opportunity to apply the skills I’ve learned in class to real-world problems and work alongside others to develop effective software solutions. Through this, I gained hands-on experience with skills that are difficult to practice on your own._
+    st.markdown("""
+            "_Bufftek has been a fantastic experience for me. It gave me the opportunity to apply the skills I’ve learned in class to real-world problems and work alongside others to develop effective software solutions. Through this, I gained hands-on experience with skills that are difficult to practice on your own._
             
-    #         _Some key areas of growth include working with other developers on the same project, managing tasks and branches to avoid conflicting changes, and testing and reviewing pull requests to ensure high-quality merges. I also learned how to collaborate with team members to choose the best technologies for each project and pitch a piece of software using a prototype to stakeholders._
+            _Some key areas of growth include working with other developers on the same project, managing tasks and branches to avoid conflicting changes, and testing and reviewing pull requests to ensure high-quality merges. I also learned how to collaborate with team members to choose the best technologies for each project and pitch a piece of software using a prototype to stakeholders._
             
-    #         _The problems I faced throughout the project ranged from finding solutions to everyday inconveniences like finding parking, to developing software for community support. This gave me practice in every phase of developing software from early research and planning, to designing the structure of the application, to building the application, to further refinement and improvement on the application. Overall, it’s been a great experience that I’d recommend to anyone wanting to get into software development or similar fields after graduation._"
+            _The problems I faced throughout the project ranged from finding solutions to everyday inconveniences like finding parking, to developing software for community support. This gave me practice in every phase of developing software from early research and planning, to designing the structure of the application, to building the application, to further refinement and improvement on the application. Overall, it’s been a great experience that I’d recommend to anyone wanting to get into software development or similar fields after graduation._"
         
-    #     """)
+        """)
 
 
-    # st.divider()
+    st.divider()
     
-    # stu2_img, stu2_text = st.columns(2)
-    # with stu2_img:
-    #     st.image("./images/StudentKim.jpg",width= 300 )
-    # with stu2_text:
-    #     st.header("Kim Sundblom")
-    #     st.subheader("Senior Undergraduate in CIS")
+    stu2_img, stu2_text = st.columns(2)
+    with stu2_img:
+        st.image("./images/StudentKim.jpg",width= 200 )
+    with stu2_text:
+        st.header("Kim Sundblom")
+        st.subheader("Senior Student in CIS")
     
 
-    # st.markdown("""
-    # _"Bufftek is amazing, as it has given me practical experience with a range of technologies while improving my teamwork and communication skills. I truly feel the club has been instrumental in boosting my confidence and preparing me for my future career by providing a supportive environment where I can apply my skills, seek guidance, and work on collaborative projects with other members. I'm really glad I stepped out of my comfort zone and joined, as it has been an extremely rewarding experience!"_
-    # """)
+    st.markdown("""
+    _"Bufftek is amazing, as it has given me practical experience with a range of technologies while improving my teamwork and communication skills. I truly feel the club has been instrumental in boosting my confidence and preparing me for my future career by providing a supportive environment where I can apply my skills, seek guidance, and work on collaborative projects with other members. I'm really glad I stepped out of my comfort zone and joined, as it has been an extremely rewarding experience!"_
+    """)
