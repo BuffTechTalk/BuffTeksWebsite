f4fd58d82b7cda7bc802f8435d19f039775e74e8
BuffTechTalk <czhang@wtamu.edu>
2024-09-30 21:43:18 -0500

    update reference page

--- webpages/reference.py
+++ webpages/reference.py
@@ -4,11 +4,12 @@ def reference():
     st.markdown("This page is used to list all references when implementing this online tutorials")
     st.markdown("""
                 
-                - [Emoji for Markdown](https://gist.github.com/rxaviers/7360908)"
+                - [Emoji for Markdown](https://gist.github.com/rxaviers/7360908)
                 - [Streamlit Cheat Sheet](https://cheat-sheet.streamlit.app/)
                 - [Navigation Bar](https://docs.streamlit.io/develop/api-reference/navigation/st.navigation)
                 - [Menu Bar](https://nicedouble-streamlitantdcomponentsdemo-app-middmy.streamlit.app/)
                 - [Bootstrap Icon](https://icons.getbootstrap.com/)
-                - [Code editor](https://okld-gallery.streamlit.app/)
+                - [Code Editor](https://okld-gallery.streamlit.app/)
+                - [Streamlit-Folium Map](https://folium.streamlit.app/)
                 
     """)
